b05c0c1edbc61541ccd6cc7998e4779097985d94
Steve Wadsworth <steve@wadsweb.com>
2020-04-18 16:58:57 +0100

    Remove HelloWorld example

--- svelte-components/src/main.js
+++ svelte-components/src/main.js
@@ -1,9 +1,4 @@
-import HelloWorld from './HelloWorld.svelte';
 import Events from './events/Events.svelte';
-
-new HelloWorld({
-  target: document.querySelector('#hello-world-container')
-});
 
 new Events({
 	target: document.querySelector('#exhibitions'),
